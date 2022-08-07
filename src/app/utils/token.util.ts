31064fd45a8cf7fba54d8b754c95a082320a493f
marcos placencio <placencio20092009@hotmail.com>
2022-08-07 14:22:06 -0400

    fix: token unused functions

--- src/app/utils/token.util.ts
+++ src/app/utils/token.util.ts
@@ -14,34 +14,29 @@ export const generateToken = (payload: any, refreshPayload:any, expiresIn:string
   return { token, refreshToken };
 }
 
-
-
-export const refreshToken = (req: Request, res: Response, next: NextFunction) => {
-
-}
 export const decodeToken = (token: string) => {
-    return jwt.decode(token, {complete: true});
+  return jwt.decode(token, {complete: true});
 }
 
 export const auth = (req: Request, res: Response, next:NextFunction) => {
-    const token = Array.isArray(req.headers?.token) ? req.headers?.token[0] : req.headers?.token;
-    const config = process.env;
+  const token = Array.isArray(req.headers?.token) ? req.headers?.token[0] : req.headers?.token;
+  const config = process.env;
 
-    if(!config.SECRET_KEY){
-        res.status(500).json({message: "Server error"});
-        return;
-    }
-  
-    if (!token) {
-      return res.status(403).send("A token is required for authentication");
-    }
+  if(!config.SECRET_KEY){
+    res.status(500).json({message: "Server error"});
+    return;
+  }
 
-    try {
-      const decoded = jwt.verify(token, config.SECRET_KEY);
-      (req as any).user = decoded;
-    } catch (err) {
-      return res.status(401).send("Invalid Token");
-    }
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
 
-    return next();
+  try {
+    const decoded = jwt.verify(token, config.SECRET_KEY);
+    (req as any).user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+
+  return next();
 };
